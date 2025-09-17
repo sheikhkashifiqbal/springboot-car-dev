467c911e4f9ab8feed174c4b06bdd73cab4f8d1e
kashif <kashusays1@gmail.com>
2025-09-17 13:43:15 +0500

    3rd milestone

--- src/main/java/com/car/carservices/controller/BranchController.java
+++ src/main/java/com/car/carservices/controller/BranchController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:3001"})
 @RequestMapping("/api/branches")
 public class BranchController {
 
